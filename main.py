0e2fff243c0ce61732a4196df1c42cb013d3ca1a
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-19 16:01:51 +0700

    refactor: rename file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routes.user import user_route
+from routes.company import user_route
 
 app = FastAPI()
 
-app.include_router(prefix="/api/v1/users", router=user_route)
+app.include_router(prefix="/api/v1/company", router=user_route)
---
+++ routes/company.py
@@ -0,0 +1,49 @@
+import json
+from typing import Any
+import uuid
+from pydantic import BaseModel
+from fastapi import APIRouter, Response, status
+from time import sleep
+
+from .db import dummy_db
+
+class Company(BaseModel):
+    _id: str | None
+    name: str
+    head_office: str
+
+user_route = APIRouter()
+
+
+def dummy_connection(key: int=-1) -> Company:
+    sleep(1) # mock database connection
+    if key == -1:
+        return dummy_db
+    return dummy_db[key]
+
+@user_route.get("/")
+def get_users() -> Response:
+    data = dummy_connection()
+    res = Response(json.dumps({"error": None, "data": data}))
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+@user_route.get("/{counter}")
+def get_users(counter: int) -> Response:
+    if counter < 0:
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+    data = dummy_connection(counter)
+    res = Response(json.dumps({"error": None, "data": data}))
+    res.headers["Content-Type"] = "application/json"
+    return res
+
+@user_route.post("/")
+def create_user(company: Company) -> Response:
+    _id = str(uuid.uuid4())
+    dummy_db.append({
+        "_id": _id,
+        "name": company.name,
+        "head_office": company.head_office
+    })
+    res = Response(json.dumps({"error": None, "message": "Created", "ObjID": _id}))
+    return res
