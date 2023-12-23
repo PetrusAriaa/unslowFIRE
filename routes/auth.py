63b79fc6b505606d53d17abe54f68daad3a3ad7c
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-23 10:45:06 +0700

    feat: handle JWT generation on success login

--- routes/auth.py
+++ routes/auth.py
@@ -1,13 +1,27 @@
-import json
-from fastapi import APIRouter, Response, status
+from datetime import timedelta, datetime
+import os
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from jose import jwt, JWTError
+
 from .db import USER_DATA
 
-auth_router = APIRouter()
 
 class UserAuth(BaseModel):
     email: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+auth_router = APIRouter()
+
 
 def find_user(email: str) -> tuple[int, bool]:
     """
@@ -19,11 +33,27 @@ def find_user(email: str) -> tuple[int, bool]:
             return i, False
     return -1, True
 
-@auth_router.post("/")
-def authentication(auth_data: UserAuth) -> Response:
-    index, is_error = find_user(auth_data.email)
+
+def generate_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, os.getenv("SECRET"), os.getenv("ALGORITHM"))
+    return token
+
+
+@auth_router.post("/", response_model=Token)
+def authentication(auth_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Response:
+    index, is_error = find_user(auth_data.username)
     if is_error:
-        return Response(json.dumps({"message" : "Data Not Found"}), status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Email address not found", headers={"WWW-Authenticate": "Bearer"})
     if auth_data.password != USER_DATA[index]["password"]:
-        return Response(json.dumps({"message" : "Unauthorized"}), status_code=status.HTTP_403_FORBIDDEN)    
-    return Response(json.dumps({"message" : "ok"}), status_code=status.HTTP_200_OK)
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Email or password is incorrect", headers={"WWW-Authenticate": "Bearer"})
+    token_expire = timedelta(minutes=float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
+    token = generate_token(data={"agent": USER_DATA[index]["username"]}, expires_delta=token_expire)
+    res = JSONResponse({"access_token" : token, "token_type": "bearer"})
+    res.headers.append("Set-Cookie", f"TOKEN={token}")
+    return res
