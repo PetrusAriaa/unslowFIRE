70360cbe1e5c288f088fa259c8f10aba09f5c818
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-23 10:44:27 +0700

    feat: CORS config

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.company import user_route
 from routes.auth import auth_router
+from dotenv import load_dotenv
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost"
+]
 
 app = FastAPI()
+load_dotenv()
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 app.include_router(prefix="/api/v1/company", router=user_route)
 app.include_router(prefix="/api/v1/auth", router=auth_router)
