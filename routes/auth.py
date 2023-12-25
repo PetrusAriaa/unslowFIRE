aa7733fab9ec103ed45b042740d249f5d4d2fba3
PetrusAriaa <petrusariacr25@gmail.com>
2023-12-26 01:14:38 +0700

    fix: oauth2_scheme url to /api/v1/auth

--- routes/auth.py
+++ routes/auth.py
@@ -1,11 +1,12 @@
-from datetime import timedelta, datetime
 import os
+from datetime import timedelta, datetime
 from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
 from jose import jwt, JWTError
+from pydantic import BaseModel
 
 from .db import USER_DATA
 
@@ -19,6 +20,8 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+
+oauth2_token_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth")
 
 auth_router = APIRouter()
 
@@ -44,6 +47,19 @@ def generate_token(data: dict, expires_delta: timedelta | None = None) -> str:
     token = jwt.encode(to_encode, os.getenv("SECRET"), os.getenv("ALGORITHM"))
     return token
 
+# Maybe used later
+def validate_token(token: Annotated[str, Depends(oauth2_token_scheme)]):
+    credential_error = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate token",
+        headers={'WWW-Authenticate': 'Bearer'}
+        )
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET"), os.getenv("ALGORITHM"))
+        print(payload)
+    except JWTError:
+        raise credential_error
+
 
 @auth_router.post("/", response_model=Token)
 def authentication(auth_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Response:
@@ -52,6 +68,8 @@ def authentication(auth_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Email address not found", headers={"WWW-Authenticate": "Bearer"})
     if auth_data.password != USER_DATA[index]["password"]:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Email or password is incorrect", headers={"WWW-Authenticate": "Bearer"})
+    if USER_DATA[index]["disabled"]:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="This user is disabled", headers={"WWW-Authenticate": "Bearer"})
     token_expire = timedelta(minutes=float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     token = generate_token(data={"agent": USER_DATA[index]["username"]}, expires_delta=token_expire)
     res = JSONResponse({"access_token" : token, "token_type": "bearer"})
